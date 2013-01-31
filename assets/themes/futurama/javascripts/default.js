5d4699c161581a1c633bbfacd63ff0ec20a08423
ermak <m.yermolovich@gmail.com>
2013-01-31 03:14:31 +0300

    add simple navigation animation (draft)

--- assets/themes/futurama/javascripts/default.js
+++ assets/themes/futurama/javascripts/default.js
@@ -1,4 +1,13 @@
 $(function() {
   $('.tooltip-enabled').tooltip();
   $('.tooltip-disabled').tooltip({ disabled: true });
+
+  $('.nav .nav-element').hover(
+    function() {
+      $(this).find('p').show('slide', 'slow');
+    },
+    function() {
+      $(this).find('p').hide('slide', 'slow');
+    }
+  );
 });
